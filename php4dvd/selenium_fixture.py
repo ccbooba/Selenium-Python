1f3d4f3d747a98b2c2a0d321fd67dd6aa0f1bf48
ccbooba <cc.booba@gmail.com>
2015-11-29 15:35:15 +0200

    STEP 5: works with application manager and user module

--- php4dvd/selenium_fixture.py
+++ php4dvd/selenium_fixture.py
@@ -1,9 +1,10 @@
 from selenium import webdriver
 import pytest
+from application import Application
 
 @pytest.fixture
-def driver(request):
+def app(request):
     driver = webdriver.Firefox()
     driver.implicitly_wait(10)
     request.addfinalizer(driver.quit)
-    return driver
+    return Application(driver)
--- php4dvd/test_remove_mov.py
+++ php4dvd/test_remove_mov.py
@@ -2,13 +2,14 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import *
-from selenium_fixture import driver
+from selenium_fixture import app
+from user import User
 
-def login(driver):
+def login(driver, user):
     driver.find_element_by_id("username").clear()
-    driver.find_element_by_id("username").send_keys("admin")
+    driver.find_element_by_id("username").send_keys(user.username)
     driver.find_element_by_name("password").clear()
-    driver.find_element_by_name("password").send_keys("admin")
+    driver.find_element_by_name("password").send_keys(user.password)
     driver.find_element_by_name("submit").click()
 
 def logout(driver):
@@ -36,29 +37,29 @@ def clear_search(driver):
     driver.find_element_by_id("q").clear()
     driver.find_element_by_id("q").send_keys(Keys.RETURN)
 
-def test_remove_mov(driver):
+def test_remove_mov(app):
     base_url = "http://localhost"
-    driver.get(base_url + "/php4dvd/")
-    login(driver)
-    clear_search(driver)
-    number_of_movies_before = number_of_movies(driver)
-    movie_name = 'Movie #1'
+    app.driver.get(base_url + "/php4dvd/")
+    login(app.driver, User.Admin())
+    clear_search(app.driver)
+    number_of_movies_before = number_of_movies(app.driver)
+    movie_name = 'Movie #5'
     """ If found, then delete the first one """
-    if number_of_movies_found(driver, movie_name) > 0:
-        remove_first_by_name(driver, movie_name)
+    if number_of_movies_found(app.driver, movie_name) > 0:
+        remove_first_by_name(app.driver, movie_name)
     else:
-        print "There is no movie with specified title: ", movie_name
+        print "\nThere is no movie with specified title: ", movie_name
 
-    number_of_movies_after = number_of_movies(driver)
+    number_of_movies_after = number_of_movies(app.driver)
 
     """ Checking if number of movies was decreased by 1 """
     if number_of_movies_after - number_of_movies_before == -1:
-        print "The movie '" + movie_name + "' was successfully removed." \
-                                           " Current number of movies: ", number_of_movies_after
+        print "\nThe movie '" + movie_name + "' was successfully removed." \
+                                           "\nCurrent number of movies: ", number_of_movies_after
     elif number_of_movies_after - number_of_movies_before == 0:
-        print "The movie was not removed. Current number of movies: ", number_of_movies_after
+        print "\nThe movie was not removed. \nCurrent number of movies: ", number_of_movies_after
     else:
-        print "Something strange happened"
+        print "\nSomething strange happened"
 
-    logout(driver)
+    logout(app.driver)
 
