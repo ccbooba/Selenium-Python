5cebca3b7f500655591126bedd701be6475cfa93
ccbooba <cc.booba@gmail.com>
2015-11-30 11:40:54 +0200

    Added second movie to Movie class

--- php4dvd/movie.py
+++ php4dvd/movie.py
@@ -1,8 +1,16 @@
 class Movie(object):
 
-    def __init__(self, movie_name=None):
+    def __init__(self, movie_name=None, year=None, notes=None):
         self.movie_name = movie_name
+        self.year = year
+        self.notes = notes
 
     @classmethod
     def Movie_first(cls):
-        return cls(movie_name="Movie #1")
+        """ This movie will be added with success """
+        return cls(movie_name="Movie #111", year="2014", notes="Worth seeing, sure!")
+
+    @classmethod
+    def Movie_second(cls):
+        """ This movie will fail to be added, as Year is blank """
+        return cls(movie_name="Movie #222", year="", notes="333")
